31f53b5616253cffba676cb90c3661b03f614b49
Sajag Subedi <sajagsubedi03@gmail.com>
2025-08-09 12:25:45 +0545

    fix email verification

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -3,22 +3,26 @@ import { ApiResponse } from "@/types/ApiResponse";
 import VerificationEmail from "../../emails/VerificationEmail";
 
 export const sendVerificationEmail = async (
-    email: string,
-    username: string,
-    verifyCode: string,
+  email: string,
+  username: string,
+  verifyCode: string
 ): Promise<ApiResponse> => {
-    try {
-        console.log(email)
-        await resend.emails.send({
-            from: "Acme <onboarding@resend.dev>",
-            to:"sajagsubedi03@gmail.com",
-            subject: "FeedBackFusion Verification email",
-            react: VerificationEmail({ username, otp: verifyCode }),
-        });
-        console.log("sent")
-        return { success: true, message: "Verification email sent" };
-    } catch (err) {
-        console.error("Error sending verification email", err);
-        return { success: false, message: "Error sending verification email" };
-    }
+  try {
+    const SENDER_EMAIL = process.env.SENDER_EMAIL;
+
+    const result = await resend.emails.send({
+      from: `FeedBackFusion <${SENDER_EMAIL}>`,
+      to: email,
+      subject: "Verify Your Email - FeedBackFusion",
+      react: VerificationEmail({
+        username,
+        otp: verifyCode,
+      }),
+    });
+
+    return { success: true, message: "Verification email sent" };
+  } catch (err) {
+    console.error("Error sending verification email", err);
+    return { success: false, message: "Error sending verification email" };
+  }
 };
